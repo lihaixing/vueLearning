f67149abc4309ec7c4d61531eb1f94181d5fd701
SeastarLI <649258445@qq.com>
2017-01-18 01:13:47 +0800

    1.第一节 实例 看看vue怎么玩

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+window.onload = function () {
+    var dataList = {
+	classValue: 'app',
+	innerText: '欢迎来到麦子学院！'
+    };
+    new Vue({
+	el: 'div',
+	data: {
+	    classValue: 'app',
+	    innerText: '欢迎来到麦子学院！'
+	}
+    });
+};
+
+
